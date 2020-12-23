d9e535cb92256fd3214ac58c0225d96c366f29b6
Hacken006 <hacken006@gmail.com>
2020-12-23 20:25:46 +0700

    vue-calculator

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+  *{
+    text-align: center;
+  }
+  body, html{
+    background-color: #21242b;
+  }
+</style>
